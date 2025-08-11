fb36f7de71902866ba35a69b9e96836b3a861b54
Gonjs <vanthien7027@gmail.com>
2025-08-11 15:10:22 +0700

    test kafkaMQ

---
+++ src/tests/kafka/producer.ts
@@ -0,0 +1,17 @@
+import { Kafka } from 'kafkajs'
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
+
+const producer = kafka.producer()
+const runProducer = async () => {
+  await producer.connect()
+  await producer.send({
+    topic: 'test-topic',
+    messages: [{ value: 'Hello KafkaJS user by GonJS' }]
+  })
+  await producer.disconnect()
+}
+runProducer().catch(console.error)
