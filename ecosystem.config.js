d2f3ec91946d0bcb7a73e25320b4e0964a4760ee
Gonjs <vanthien7027@gmail.com>
2025-09-03 14:02:45 +0700

    tôi thử lại lần n

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+// ecosystem.config.js
+module.exports = {
+  apps: [
+    {
+      name: 'shopDev-be',
+      script: './dist/index.js', // Đường dẫn tới file JS sau khi build
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G'
+    }
+  ]
+}
