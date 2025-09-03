6f94a25222bf20ca82f6499e1f08015dda60d862
Gonjs <vanthien7027@gmail.com>
2025-09-03 10:04:57 +0700

    FIX: node.yaml 002

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -1,10 +1,13 @@
+// ecosystem.config.cjs
 module.exports = {
   apps: [
     {
       name: 'shopDev-be',
-      script: 'dist/index.js',
-      node_args: '--require tsconfig-paths/register',
-      cwd: '/home/ubuntu/actions-runner/_work/be-e-commerce/be-e-commerce', // <== cập nhật đúng path repo của bạn
+      script: 'dist/index.js', // chạy file build JS
+      cwd: '/home/ubuntu/actions-runner/_work/be-e-commerce/be-e-commerce',
+      exec_mode: 'cluster',
+      interpreter: 'node', // QUAN TRỌNG: dùng node, không phải bun
+      // node_args: '',                        // không cần nếu chạy dist/*
       env: { NODE_ENV: 'production' }
     }
   ]
